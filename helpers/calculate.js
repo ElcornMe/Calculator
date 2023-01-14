4549a30b6a2886397b97d9343442c0b14312f28e
ElcornMe <elcornme@gmail.com>
2023-01-14 13:09:48 +0200

    "change method toFixed from 9 to 8"

--- helpers/calculate.js
+++ helpers/calculate.js
@@ -1,21 +1,21 @@
 function calculate( operand, numberOne, numberTwo) {
 
   if(operand === '+') {
-    return parseFloat((Number(numberOne) + Number(numberTwo)).toFixed(9)) || "Mistake";
+    return parseFloat((Number(numberOne) + Number(numberTwo)).toFixed(8)) || "Mistake";
   }
 
   if(operand === '-') {
-    return parseFloat((Number(numberOne) - Number(numberTwo)).toFixed(9)) || "Mistake";
+    return parseFloat((Number(numberOne) - Number(numberTwo)).toFixed(8)) || "Mistake";
   }
 
   if(operand === '÷') {
     if(numberTwo === '0') {
       return "Prohibit"
     }
-    return parseFloat((Number(numberOne) / Number(numberTwo)).toFixed(9)) || "Mistake";
+    return parseFloat((Number(numberOne) / Number(numberTwo)).toFixed(8)) || "Mistake";
   }
 
   if(operand === '∗') {
-    return parseFloat((Number(numberOne) * Number(numberTwo)).toFixed(9)) || "Mistake";
+    return parseFloat((Number(numberOne) * Number(numberTwo)).toFixed(8)) || "Mistake";
   }
 };
